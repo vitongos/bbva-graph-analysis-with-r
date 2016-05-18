9e976488427ea380f37d5c35b40f080b383a77b8
Victor <victorsvv@yahoo.com>
2016-05-18 08:35:26 +0200

    Install map libs.

--- deploy/r.sh
+++ deploy/r.sh
@@ -4,7 +4,7 @@ cd ~/Downloads
 sudo su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm'
 sudo yum -y update
 sudo yum -y install R
-sudo su -c 'rpm -Uhv https://download1.rstudio.org/rstudio-0.99.902-x86_64.rpm'
-sudo yum -y install mesa-libGL-devel mesa-libGLU-devel libpng-devel
+sudo su -c 'rpm -Uhv https://download1.rstudio.org/rstudio-0.97.320-x86_64.rpm'
+sudo yum -y install mesa-libGL-devel mesa-libGLU-devel libpng-devel libjpeg-devel
 sudo yum -y install libcurl libcurl-devel
 sudo yum -y install libxml2-devel curl openssl-devel libssh2-devel
